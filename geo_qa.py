9521ab71ca89e078f37500f6d37ebcf2c8aa27a9
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-04-20 12:12:59 +0300

    Update geo_qa.py

--- geo_qa.py
+++ geo_qa.py
@@ -41,7 +41,7 @@ def create_graph():
 
 
 def add_entities_to_graph(g, countries_urls):
-    for country_url in countries_urls[35:40]:
+    for country_url in countries_urls:
         country_name = country_url.split("/")[-1]
         print(country_name)
         r = requests.get(country_url)
