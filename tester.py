52865402f5b41211785ce7b4ea2ea0372124124b
yuvalmor96 <65467427+yuvalmor96@users.noreply.github.com>
2022-04-29 17:42:09 +0300

    tester

---
+++ tester.py
@@ -0,0 +1,157 @@
+import sys
+import os
+import geo_qa
+
+
+if __name__ == '__main__':
+
+    # print("activate: python geo_qa.py create ontology.nt")
+    # os.system("python geo_qa.py create ontology.nt")
+
+
+    print("\nquestion 1: Who is the president of China?")
+    print("Xi Jinping")
+    os.system('python geo_qa.py question "Who is the president of China?"')
+
+    print("\nquestion 2: Who is the president of Portugal?")
+    print("Marcelo Rebelo de Sousa")
+    os.system('python geo_qa.py question "Who is the president of Portugal?"')
+
+    print("\nquestion 3: Who is the president of Guam?")
+    print("Joe Biden")
+    os.system('python geo_qa.py question "Who is the president of Guam?"')
+
+    print("\nquestion 4: Who is the prime minister of Eswatini?")
+    print("Cleopas Dlamini")
+    os.system('python geo_qa.py question "Who is the prime minister of Eswatini?"')
+
+    print("\nquestion 5:Who is the prime minister of Tonga?")
+    print("Siaosi Sovaleni")
+    os.system('python geo_qa.py question "Who is the prime minister of Tonga?"')
+
+    print("\nquestion 6: What is the population of Isle of Man?")
+    print("84,069")
+    os.system('python geo_qa.py question "What is the population of Isle of Man?"')
+
+    print("\nquestion 7: What is the population of Tokelau?")
+    print("1,499")
+    os.system('python geo_qa.py question "What is the population of Tokelau?"')
+
+    print("\nquestion 8:What is the population of Djibouti?")
+    print("921,804")
+    os.system('python geo_qa.py question "What is the population of Djibouti?"')
+
+    print("\nquestion 9:What is the area of Mauritius?")
+    print("2,040 km squared")
+    os.system('python geo_qa.py question "What is the area of Mauritius?"')
+
+    print("\nquestion 10:What is the area of Luxembourg?")
+    print("2,586.4 km squared")
+    os.system('python geo_qa.py question "What is the area of Luxembourg?"')
+
+    print("\nquestion 11:What is the area of Guadeloupe?")
+    print("1,628 km squared ")
+    os.system('python geo_qa.py question "What is the area of Guadeloupe?"')
+
+    print("\nquestion 12:What is the form of government in Argentina?")
+    print("Federal republic, Presidential system, Republic")
+    os.system('python geo_qa.py question "What is the form of government in Argentina?"')
+
+    print("\nquestion 13:What is the form of government in Sweden?")
+    print("Constitutional monarchy, Parliamentary system, Unitary state")
+    os.system('python geo_qa.py question "What is the form of government in Sweden?"')
+
+    print("\nquestion 15:What is the form of government in North Macedonia?")
+    print("Parliamentary republic, Unitary state")
+    os.system('python geo_qa.py question "What is the form of government in North Macedonia?"')
+
+    print("\nquestion 1:What is the capital of Burundi?")
+    print("Gitega")
+    os.system('python geo_qa.py question "What is the capital of Burundi?"')
+
+    print("\nquestion 17:What is the capital of Mongolia?")
+    print("Ulaanbaatar")
+    os.system('python geo_qa.py question "What is the capital of Mongolia?"')
+
+    print("\nquestion 18:What is the capital of Andorra?")
+    print("Andorra la Vella")
+    os.system('python geo_qa.py question "What is the capital of Andorra?"')
+
+    print("\nquestion 19:What is the capital of Saint Helena, Ascension and Tristan da Cunha?")
+    print("Jamestown, Saint Helena")
+    os.system('python geo_qa.py question "What is the capital of Saint Helena, Ascension and Tristan da Cunha?"')
+
+    print("\nquestion 20:What is the capital of Greenland?")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+
+    print("\nquestion 20:List all countries whose capital name contains the string hi")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+
+    print("\nquestion 20:What is the capital of Greenland?")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+    
+    print("\nquestion 20:What is the capital of Greenland?")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+    
+    print("\nquestion 20:What is the capital of Greenland?")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+    
+    print("\nquestion 20:What is the capital of Greenland?")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+    
+    print("\nquestion 20:What is the capital of Greenland?")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+    
+    print("\nquestion 20:What is the capital of Greenland?")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+    
+    print("\nquestion 20:What is the capital of Greenland?")
+    print("Nuuk")
+    os.system('python geo_qa.py question "What is the capital of Greenland?"')
+
+    # print("question 1: who is the president of Italy?")
+    # os.system('python geo_qa.py question "who is the president of Italy?"')
+    # print("\nquestion 2: who is the prime minister of United Kingdom?")
+    # os.system('python geo_qa.py question "who is the prime minister of United Kingdom?"')
+    # print("\nquestion 3: what is the population of Democratic Republic of the Congo?")
+    # os.system('python geo_qa.py question "what is the population of Democratic Republic of the Congo?"')
+    # print("\nquestion 4 what is the area of Fiji?:")
+    # os.system('python geo_qa.py question "what is the area of Fiji?"')
+    # print("\nquestion 5 What is the government of Eswatini?:")
+    # os.system('python geo_qa.py question "What is the government of Eswatini?"')
+    # print("\nquestion 6 what is the capital of Canada?:")
+    # os.system('python geo_qa.py question "what is the capital of Canada?"')
+    # print("\nquestion 7 when was the president of South Korea born?:")
+    # os.system('python geo_qa.py question "when was the president of South Korea born?"')
+    # print("\nquestion 8 when was the prime minister of New Zealand born?:")
+    # os.system('python geo_qa.py question "when was the prime minister of New Zealand born?"')
+    # print("\nquestion 9: who is Donald Trump?")
+    # os.system('python geo_qa.py question "who is Donald Trump?"')
+    # print("\nquestion 10: who is kyriakos mitsotakis?")
+    # os.system('python geo_qa.py question "who is Kyriakos Mitsotakis?"')
+
+    # print("\nquestion 1: Who is the pResident of itAly?")
+    # os.system('python geo_qa.py question "Who is the pResident of itAly?"')
+
+    # print("\nquestion 3: what is the PopulaTion of Democratic republic Of the CoNgo?")
+    # os.system('python geo_qa.py question "what is the population of Democratic Republic of the Congo?"')
+
+
+    # print("\nNull return question 11: who is the president of United States Virgin Islands?")
+    # os.system('python geo_qa.py question "who is the president of United States Virgin Islands?"')  
+    
+    # print("\nNull return question 12: what is the capital of Monaco?")
+    # os.system('python geo_qa.py question "what is the capital of Monaco?"')
+    
+    # print("\nquestion 2: who is the prime minister of east timor?")
+    # os.system('python geo_qa.py question "who is the prime minister of east timor?"')
+    # print("")
+
